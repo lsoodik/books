50b49c6c0d1d4ecc07140bb510dabc137cd7a8bd
lsoodik <ssonya2311@gmail.com>
2024-12-03 13:19:10 +0700

    remade pug file

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 import "./index.css";
 import "./slider.js"
+import "./getBooks.js"
--- src/slider.js
+++ src/slider.js
@@ -13,6 +13,7 @@ function initSlider () {
 
   initImages();
   initCircles();
+  initAutoPlay();
 
   function initImages() {
     images.forEach((image, index) => {
@@ -36,6 +37,15 @@ function initCircles() {
         });
     });
 }
+
+function initAutoPlay(){
+  setInterval(()=>{
+    let curNum = +sliderImages.querySelector(".active").dataset.index;
+    let nextNum = curNum === images.length - 1? 0 : curNum + 1;
+    moveSlider(nextNum)
+  }, 5000)
+}
+
   function moveSlider(num) {
       sliderImages.querySelector(".active").classList.remove("active");
       sliderImages.querySelector(".n" + num).classList.add("active");
